56c4cf0b5361b277f0e83d045f5924953e245589
Anas Iqbal <theanasiqball@gmail.com>
2024-11-12 12:49:24 +0530

    CHANGED VERCEL URL to baseurl

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -7,7 +7,7 @@ type Value<T> = T extends Promise<infer U> ? U : T;
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.BASE_URL) return `https://${process.env.BASE_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use locaslhost
 }
 
